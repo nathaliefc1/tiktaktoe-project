33442198a3452fb3742737ba1ff76763a1d0bf56
Nathalie Fajardo <nathaliefc89@gmail.com>
2023-11-24 19:12:42 +0100

    starting project tictactoe

---
+++ src/components/Player.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+  
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+  }
+  function handleChange(event) {
+    console.log(event);
+    setPlayerName(event.target.value);
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {!isEditing ? (
+          <span className="player-name">{playerName}</span>
+        ) : (
+          <input type="text" value={playerName} onChange={handleChange}/>
+        )}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
